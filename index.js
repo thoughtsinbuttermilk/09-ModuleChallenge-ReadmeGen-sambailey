b6f5be1301ed0e5ea663e5ca28a4d93ac22f3f56
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-03 17:45:02 -0500

    implemented greeting and array of questions to collect user input

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const inquirer = require('inquirer');
 // include required markdownDynamo js utilities
 const mdDynamo = require('./utils/markdownDynamo');
 
+// greeting
+console.log('Hello. I am the README Dynamo. Let me help you make a professional README.md file')
+
 // TODO: Create an array of questions for user input
-const userQuestions = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type: 'input',
-            title: 'title',
+            name: 'title',
+            // default: 'this string will be used to set the title of your project',
             message: 'please enter the title of your project: ',
         },
         {
@@ -42,22 +45,25 @@ const userQuestions = () => {
             name: 'tests',
             message: 'please enter instructions for testing this application:',
         },
-    ]);
-};
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log('\readme information');
+    console.log(JSON.stringify(answers, null, '  '));
+  });
 
 // TODO: Create a function to write README file
-const writeFile = ({name, title, description, installation, usage, contributions, tests}) => 
-`
-this is a test
-the project title is ${title}
-`
+// const writeFile = ({name, title, description, installation, usage, contributions, tests}) => 
+// `
+// this is a test
+// the project title is ${title}
+// `
 
 // TODO: Create a function to initialize app
-const init() => { 
-    userQuestions()
-    .then((answers)) => writeFile
-}
+// const init() => { 
+//     userQuestions()
+//     .then((answers)) => writeFile
+// }
 
 // Function call to initialize app
-// temp: init userQuestions
-init(userQuestions);
+// init();

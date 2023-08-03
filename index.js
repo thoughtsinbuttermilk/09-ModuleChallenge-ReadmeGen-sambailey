e93873a09088345e1d860bdd5738dae19ff64488
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-03 15:39:16 -0500

    created an array of questions for user input

--- index.js
+++ index.js
@@ -10,13 +10,46 @@ const inquirer = require('inquirer');
 const mdDynamo = require('./utils/markdownDynamo');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const userQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            title: 'title',
+            message: 'please enter the title of your project: ',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'please enter a descrption for your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'please enter installation instructions:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'please enter instructions for using this application:',
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'please enter instructions for contributing to this repository and application:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'please enter instructions for testing this application:',
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();

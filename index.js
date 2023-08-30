19cc4d88dc60e133a1ca948bfbc66013d97dec20
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-30 18:07:48 -0500

    added welcome message and instructions to quit to init()

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ const questions = require('./utils/userInput');
 // TODO: write a function to initialize app
 
 function init() {
+
+    console.log(`
+    =============================
+    
+    welcome to the README Dynamo! 
+    answer the following question
+    prompts to feed information
+    to the generator
+
+    press ctrl+c to quit
+
+    =============================
+    `);
+
     inquirer 
     .prompt(questions)
     // .then((response) => {

a12db03686ebe073bb6a3e2a10e00772330e352b
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-30 17:32:25 -0500

    added skeleton of init, updated required packages

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// required
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+const questions = require('./utils/userInput');
 
 // goal: compartmentalize the functions of this application
 // TODO: write a function to write md to file
 // TODO: write a function to initialize app
+
+function init() {
+    inquirer 
+    .prompt(questions)
+    // .then((response) => {
+    //     writeToFile(response.fileName, response);
+    // })
+    // .catch(err => {
+    //     console.log(err)
+    // });
+}
+// initialize the application (after it exists!)
+init();

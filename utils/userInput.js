4da01325ad8df78759b1c1a6b80f5045258b9000
thoughtsinbuttermilk <thoughtsinbuttermilk@gmail.com>
2023-08-30 16:58:12 -0500

    added prompts for license, github username, github profile, e-mail address, formatting

--- utils/userInput.js
+++ utils/userInput.js
@@ -1,106 +1,152 @@
 // TODOS: requirements from readme.md
-// DONE: when I enter my project title, then this is displayed as the title of the README
-// DONE: when I enter a description, installation instructions, usage information, contribution guidelines, and test instructions, then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// when I choose a license for my application from a list of options, then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// when I enter my GitHub username, then this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// when I enter my email address, then this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// when I click on the links in the Table of Contents
+// DONE: user input for project title, description, installation instructions, usage information,
+// contribution guidelines, and test instructions, GitHub username, link to my GitHub profile, email address
+
+const { default: separator } = require("inquirer/lib/objects/separator");
+
+// DONE: find something to validate the e-mail address
+// NOTE: the e-mail address really shouldn't be validated on the front end but, hey...
+const validator = require("email-validator");
 
 const questions = [
-    {
-        // project title
-        name: 'title',
-        message: '(required) enter the name of your project:',
-        type: 'input',
-        // validate: title is required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter the project title");
-            }
-            return true;
-        }
+  {
+    // project title
+    name: "title",
+    message: "(required) enter the name of your project:",
+    type: "input",
+    // validate: title is required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter the project title");
+      }
+      return true;
     },
+  },
 
-    {
-        // project description
-        name: 'description',
-        message: '(required) enter a description of your project:',
-        type: 'input',
-        // validate: description is required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter the project description");
-            }
-            return true;
-        }
+  {
+    // project description
+    name: "description",
+    message: "(required) enter a description of your project:",
+    type: "input",
+    // validate: description is required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter the project description");
+      }
+      return true;
     },
+  },
 
-    {
-        // installation instructions
-        name: 'instalInstructions',
-        message: '(required) enter installation instructions:',
-        type: 'input',
-        // validate: installation instructions are required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter the installation instructions");
-            }
-            return true;
-        }
+  {
+    // installation instructions
+    name: "instalInstructions",
+    message: "(required) enter installation instructions:",
+    type: "input",
+    // validate: installation instructions are required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter the installation instructions");
+      }
+      return true;
     },
+  },
 
-    {
-        // usage information
-        name: 'usage',
-        message: '(required) enter usage instructions:',
-        type: 'input',
-        // validate: usage instructions are required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter the usage instructions");
-            }
-            return true;
-        }
+  {
+    // usage information
+    name: "usage",
+    message: "(required) enter usage instructions:",
+    type: "input",
+    // validate: usage instructions are required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter the usage instructions");
+      }
+      return true;
     },
+  },
 
-    {
-        // contribution guidelines
-        name: 'contribution',
-        message: '(required) enter contribution instructions:',
-        type: 'input',
-        // validate: contribution instructions are required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter contribution instructions");
-            }
-            return true;
-        }
+  {
+    // contribution guidelines
+    name: "contribution",
+    message: "(required) enter contribution instructions:",
+    type: "input",
+    // validate: contribution instructions are required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter contribution instructions");
+      }
+      return true;
     },
+  },
 
-    {
-        // test information
-        name: 'testing',
-        message: '(required) enter testing instructions:',
-        type: 'input',
-        // validate: testing instructions are required
-        validate: (answer) => {
-            if (answer === "") {
-                return console.log("\n please enter testing instructions");
-            }
-            return true;
-        }
+  {
+    // test information
+    name: "testing",
+    message: "(required) enter testing instructions:",
+    type: "input",
+    // validate: testing instructions are required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter testing instructions");
+      }
+      return true;
     },
+  },
 
+  {
+    // license (added to project description)
+    name: "license",
+    message: "select a license for your project:",
+    type: "list",
+    choices: [
+      "GNU v3",
+      inquirer.Separator(),
+      "MIT",
+      inquirer.Separator(tick),
+      "Creative Commons Zero v1.0 Universal",
+      inquirer.Separator(cross),
+      "Mozilla Public License 2.0",
+      inquirer.Separator(),
+      "The Unlicense",
+    ],
+  },
 
-    // license choice
-
-    // license badge (added to project description)
-
-    // external link to license
-
+  {
     // github username
+    name: "ghUserName",
+    message: "(required) enter your github username:",
+    type: "input",
+    // validate: github username is required
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter your github username");
+      }
+      return true;
+    },
+  },
 
-    // email address
+  {
+    // github user profile
+    name: "ghUserName",
+    message: "(required) enter the URL of your github profile:",
+    type: "input",
+    // validate: github profile is required
+    // not validating the url
+    validate: (answer) => {
+      if (answer === "") {
+        return console.log("\n please enter the URL of your github profile");
+      }
+      return true;
+    },
+  },
 
-    // alternate contact information
-]
+  {
+    // e-mail address
+    name: "emailAddress",
+    message: "(required) enter your e-mail address:",
+    type: "input",
+    // validate: e-mail address is required
+    // let's see if regex or something else would be good to validate the form of the address, if not the address itself.
+    validate: emailValidator,
+  },
+];
